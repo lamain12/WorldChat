9ed10c15658d0d0a1674290ed1df671486973d04
darren <long0015@student.monash.edu>
2025-07-31 10:51:12 +0800

    feat: setted up the websocket connection

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 
-api = FastAPI()
+app = FastAPI()
 clients = []
 
 @app.get("/")
 async def get():
     return HTMLResponse(open("templates/index.html").read())
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # Broadcasr to all connected clients
+            for client in clients:
+                await client.send_text(data)
+    except WebSocketDisconnect:
+        clients.remove(websocket)
 
